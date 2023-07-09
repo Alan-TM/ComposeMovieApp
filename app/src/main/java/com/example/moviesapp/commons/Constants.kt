b1a5a59905977e015b2a357bdf46dae785cedb70
Alan Torres Morales <alantorresm86@gmail.com>
2023-07-09 12:06:50 -0700

    feat: retrofit module config is added

---
+++ app/src/main/java/com/example/moviesapp/commons/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.moviesapp.commons
+
+object Constants {
+    internal const val BASE_URL = "https://api.themoviedb.org/3/"
+    internal const val MOVIE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w200//"
+}
